101cad64cc7179ef62303d1361f69760c6320749
sureshYanamula <ysureshy.1994@gmail.com>
2023-12-30 22:01:28 +0530

    added solution for left totate array by k places leetcode 189 problem

---
+++ Arrays/Left_Rotate_K_Places_189.js
@@ -0,0 +1,22 @@
+function rotate(nums, k) {
+  k = k % nums.length; // Reduce k to the effective rotation value within array length
+
+  // Reverse the entire array
+  reverse(nums, 0, nums.length - 1);
+
+  // Reverse the first k elements
+  reverse(nums, 0, k - 1);
+
+  // Reverse the remaining elements after k
+  reverse(nums, k, nums.length - 1);
+}
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    const temp = nums[start];
+    nums[start] = nums[end];
+    nums[end] = temp;
+    start++;
+    end--;
+  }
+}
